66a26e53be62b960d3ca8d19fa893189ce15f08e
DARP7 <danel_bmt@hotmail.com>
2016-05-24 09:49:22 -0500

    cambios a consulta artesano.php

--- artesanos/consultaArtesanos.php
+++ artesanos/consultaArtesanos.php
@@ -23,61 +23,70 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         <link rel="stylesheet" href="../css/bootstrap.css">
     </head>
     <body>
-    <div id="header">
-        Este es el header
-    </div>
-    <div id="nav">
-        <ul>
-            <li><a href="#Artesanias">Artesanias</a></li>
-            <li><a href="artesanosPrincipal.php">Artesanos</a>
-                <ul>
-                    <li><a href="#ReporteArtesanos">ReporteArtesanos</a></li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-    <div id="search">
-        <form action="consultaArtesanos.php" method="get">
-            <input type="search" name="codigo"> <input type="submit" value="Buscar">
-        </form>
-    </div>
-    <div id="table">
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>NOMBRE</th>
-                <th>REGION</th>
-                <th>BIOGRAFÍA</th>
-                <th>DIRECCIÓN</th>
-                <th>TELÉFONO</th>
-            </tr>
-            <?php
-            if ($result = $connection->query($query)) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $row['idArtesano'] . "</td>";
-                    echo "<td>" . $row['nombre'] . "</td>";
-                    echo "<td>" . $row['region'] . "</td>";
-                    echo "<td>" . $row['bio'] . "</td>";
-                    echo "<td>" . $row['direccion'] . "</td>";
-                    echo "<td>" . $row['telefono'] . "</td>";
-                    echo "</tr>";
-                }
-            }
-            $result->free();
-            $connection->close();
-            ?>
-        </table>
-    </div>
-    <div id="buttons">
-        <button type="button">Eliminar</button>
-        <button type="button">Editar</button>
-        <a href="agregar.php"><button type="button">Agregar</button></a>
-    </div>
-    <div id="footer">
-        Este es el footer
-    </div>
-
+    <div class="container">
+	   <div class="container-fluid">
+		<div class="row">
+		
+			<img src="../img/descarga.png" class="img-responsive">
+		
+	   	</div>
+	   </div>
+	   <div class="navbar navbar-inverse">
+		    <div class="container-fluid">
+			    <ul class="nav navbar-nav">
+				<li><a href="#Artesanias">Artesanias</a></li>
+				<li class="dropdown">
+				<a class="dropdown-toggle" data-toggle="dropdown" href="artesanoPrincipal.php">Artesanos
+				<span class="caret"></span></a>
+				    <ul class="dropdown-menu">
+					<li><a href="#ReporteArtesanos">ReporteArtesanos</a></li>
+				    </ul>
+				</li>
+			    </ul>
+		    </div>
+	    </div>
+	    <div class="text-right">
+		    <form action="consultaArtesanos.php" method="get">
+			<input type="search" name="codigo"> <input class="btn btn-info" type="submit" value="Buscar">
+		    </form>
+	    </div>
+	    <div id="table">
+		<table>
+		    <tr>
+		        <th>ID</th>
+		        <th>NOMBRE</th>
+		        <th>REGION</th>
+		        <th>BIOGRAFÍA</th>
+		        <th>DIRECCIÓN</th>
+		        <th>TELÉFONO</th>
+		    </tr>
+		    <?php
+		    if ($result = $connection->query($query)) {
+		        while ($row = $result->fetch_assoc()) {
+		            echo "<tr>";
+		            echo "<td>" . $row['idArtesano'] . "</td>";
+		            echo "<td>" . $row['nombre'] . "</td>";
+		            echo "<td>" . $row['region'] . "</td>";
+		            echo "<td>" . $row['bio'] . "</td>";
+		            echo "<td>" . $row['direccion'] . "</td>";
+		            echo "<td>" . $row['telefono'] . "</td>";
+		            echo "</tr>";
+		        }
+		    }
+		    $result->free();
+		    $connection->close();
+		    ?>
+		</table>
+	    </div>
+	    <div id="buttons">
+		<button type="button">Eliminar</button>
+		<button type="button">Editar</button>
+		<a href="agregar.php"><button type="button">Agregar</button></a>
+	    </div>
+	    <div id="footer">
+		Este es el footer
+	    </div>
+    </div>	
     </body>
 
     </html>
