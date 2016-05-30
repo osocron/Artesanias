595c63679a82dfe7b5c5e8cfccd5f86995b78c5c
DARP7 <danel_bmt@hotmail.com>
2016-05-30 14:30:45 -0500

    cambios

--- artesanos/agregar.php
+++ artesanos/agregar.php
@@ -84,7 +84,7 @@
             ?>
             <tr>
                 <td><input class="form-control input-sm" type="TEXT" name="id"></td>
-                <td><input class="form-control input-sm" type="text" name="nombre"></td>
+                <td><input class="form-control input" type="text" name="nombre"></td>
                 <td><input type="text" name="region"></td>
                 <td><input type="text" name="bio"></td>
                 <td><input type="text" name="direccion"></td>
